59684157d158679a20a1bbeb06ddd2fa2d508b82
Dhanaji Metil <djmcatechpro@gmail.com>
2024-01-27 19:39:17 -0500

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by DHANAJI METIL on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
